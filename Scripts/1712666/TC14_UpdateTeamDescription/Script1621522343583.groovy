1952a723fb29a0af0391ad88c9631b27c9d89b9d
Phuc <hvphuc081299@gmail.com>
2021-05-20 22:29:03 +0700

    create TC14 UpdateTeamDescription

---
+++ Scripts/1712666/TC14_UpdateTeamDescription/Script1621522343583.groovy
@@ -0,0 +1,46 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.exception.StepErrorException
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://mattermostwebapp.herokuapp.com/')
+
+WebUI.callTestCase(findTestCase('1712666/CommonTC/CTC_Login'), [('var_username'): 'phuc', ('var_password'): 'CSt51UwWECc='])
+
+WebUI.click(findTestObject('Object Repository/Page_Mattermost/button_Sign in (1)'))
+
+WebUI.click(findTestObject('Object Repository/Page_Town Square - test2 Mattermost/div_phuc'))
+
+WebUI.click(findTestObject('Object Repository/Page_Town Square - test2 Mattermost/span_Team Settings'))
+
+WebUI.click(findTestObject('Object Repository/Page_Town Square - test2 Mattermost/span_Edit'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Town Square - test2 Mattermost/input_Team Description_teamDescription'), 
+    var_teamDescription)
+
+WebUI.click(findTestObject('Object Repository/Page_Town Square - test2 Mattermost/span_Save'))
+
+String newTeamDescription = WebUI.getText(findTestObject('Object Repository/Page_Town Square - test2 Mattermost/div_test'))
+
+if (newTeamDescription != var_teamDescription.toString()) {
+	throw new StepErrorException('Fail updated team description')
+}
+
+WebUI.closeBrowser()
