d3596b748fa719125a69fa48208fbffd89518db3
Phuc <hvphuc081299@gmail.com>
2021-05-20 15:27:08 +0700

    create TC11_CreateATeam

---
+++ Test Suites/1712666/TS TC11 CreateATeam.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS TC11 CreateATeam</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f5c2b923-17b2-4349-9763-85b0bd7291e8</testSuiteGuid>
+   <testCaseLink>
+      <guid>21c92b5e-3f7c-4c74-b70d-f40c5cd3977b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1712666/TC11_CreateATeam</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>aa45f1d9-2be4-4746-b60c-373d500c3695</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/1712666/Data TC11 CreateATeam</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>aa45f1d9-2be4-4746-b60c-373d500c3695</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>team name</value>
+         <variableId>f76a89a8-067d-4083-bebd-cef7025107fc</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>aa45f1d9-2be4-4746-b60c-373d500c3695</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>status expected</value>
+         <variableId>c0f64aa5-b79c-401b-b090-81bc781dd876</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
